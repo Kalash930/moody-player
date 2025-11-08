5f7c3348108df990d916bf3d07ea860fa01138c0
Kalash Verma <kalashverma930555@gmail.com>
2025-11-08 13:11:52 +0530

    fix: added CORS for Vercel frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,15 @@ const cors=require('cors')
 
 const app=express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173", 
+    "https://moody-player-theta-seven.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use('/',songRoutes)
 
 module.exports=app
